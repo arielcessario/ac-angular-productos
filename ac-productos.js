8033b5fc63591eb74d4f4f1d36b677f8f3533dcc
Mateo Maneff <mmaneff@gmail.com>
2015-11-27 07:58:49 -0200

    Fix

--- ac-productos.js
+++ ac-productos.js
@@ -4,6 +4,10 @@
     var scripts = document.getElementsByTagName("script");
     var currentScriptPath = scripts[scripts.length - 1].src;
 
+    if(currentScriptPath.length == 0){
+        currentScriptPath = window.installPath + '/ac-angular-productos/includes/ac-productos.php';
+    }
+
     angular.module('acProductos', [])
         .factory('ProductService', ProductService)
         .service('ProductVars', ProductVars)
